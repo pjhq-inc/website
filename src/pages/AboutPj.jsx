827f3c99b543f3cdbdac407e2227d08c0fd86f9c
god <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-03-14 19:13:27 +0100

    Create AboutPj.jsx

---
+++ src/pages/AboutPj.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}> //gayys!! i centered a div!!! :clueless:
+      <h1>About Us</h1>
+      <p>quack.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
