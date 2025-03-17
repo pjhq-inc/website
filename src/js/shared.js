694077714f9ca11e2a05318c9a50a5d96d16d94e
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2025-03-17 19:03:38 +0200

    bleh

---
+++ src/js/shared.js
@@ -0,0 +1,13 @@
+
+//shit so ass yet none of you can stop me
+
+document.write('\
+    <div id="top-bar-div">\
+    <ul id="top-bar">\
+        <li id="bar-btn"><a href="https://pjhq.dev"><i class="material-icons" style="color: rgb(0, 227, 102);">home</i> Home</a></li>\
+        <li id="bar-btn"><a href="https://pjhq.dev/about"><i class="material-icons" style="color: rgb(0, 79, 227);">info</i> About</a></li>\
+        <li id="bar-btn"><a href="https://ko-fi.com/pjhqdev"><i class="material-icons" style="color: rgb(227, 0, 0);">favorite</i> Support</a></li>\
+        <li id="bar-btn"><a href="https://pjhq.dev/store"><i class="material-icons" style="color: rgb(166, 227, 0);">store</i> Store</a></li>\
+        <li id="bar-btn"><a href="https://pjhq.dev/projects"><i class="material-icons" style="color: rgb(255, 255, 255);">build</i> Projects</a></li>\
+    </ul>\
+</div>');
