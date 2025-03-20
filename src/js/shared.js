b1efde6a495c5f799e287815bfeef2782767c64b
Pixelated Face <105989373+PixelatedFace@users.noreply.github.com>
2025-03-20 02:04:17 +0200

    Update shared.js

--- src/js/shared.js
+++ src/js/shared.js
@@ -7,7 +7,7 @@ document.write('\
         <li id="bar-btn"><a href="https://pjhq.dev"><i class="material-icons" style="color: rgb(0, 227, 102);">home</i> Home</a></li>\
         <li id="bar-btn"><a href="https://pjhq.dev/about"><i class="material-icons" style="color: rgb(0, 79, 227);">info</i> About</a></li>\
         <li id="bar-btn"><a href="https://ko-fi.com/pjhqdev"><i class="material-icons" style="color: rgb(227, 0, 0);">favorite</i> Support</a></li>\
-        <li id="bar-btn"><a href="https://pjhq.dev/store"><i class="material-icons" style="color: rgb(166, 227, 0);">store</i> Store</a></li>\
+        <li id="bar-btn"><a href="https://www.amazon.com/hz/wishlist/ls/160E4F8L5H81K?ref_=wl_share"><i class="material-icons" style="color: rgb(166, 227, 0);">store</i> Store</a></li>\
         <li id="bar-btn"><a href="https://pjhq.dev/projects"><i class="material-icons" style="color: rgb(255, 255, 255);">build</i> Projects</a></li>\
     </ul>\
 </div>');
